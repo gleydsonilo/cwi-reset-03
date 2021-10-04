b410c1968c06c872b600b5950542356d8b9fd3b7
Gleydsonilo Negre <gleydsontaichou@gmail.com>
2021-10-04 16:13:54 -0300

    Refatora a classe ItensPorQuantidade

--- desafio-aula-01/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
+++ desafio-aula-01/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
@@ -6,25 +6,4 @@ public class ItensPorQuantidade {
     static int leite = 20;
     static int cafe = 20;
 
-    public static void reduzItemEstoque(String item, int qtd) {
-        if ("pao".equals(item)) {
-            ItensPorQuantidade.pao -= (60 - qtd);
-        }
-
-        if ("torta".equals(item)) {
-            ItensPorQuantidade.torta -= qtd;
-        }
-
-        if ("leite".equals(item)) {
-            ItensPorQuantidade.leite -= qtd;
-        }
-
-        if ("cafe".equals(item)) {
-            ItensPorQuantidade.cafe -= qtd;
-        }
-
-        if ("sanduiche".equals(item)) {
-            ItensPorQuantidade.sanduiche -= qtd;
-        }
-    }
 }
